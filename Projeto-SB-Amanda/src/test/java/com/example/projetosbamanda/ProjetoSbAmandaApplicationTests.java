b36ea895d0e56811492f056417996cf6bc092b83
Amanda Galindo <amandagalindo907@gmail.com>
2024-03-12 11:34:07 -0300

    Init

---
+++ Projeto-SB-Amanda/src/test/java/com/example/projetosbamanda/ProjetoSbAmandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetosbamanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSbAmandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
